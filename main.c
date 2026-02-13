ac98757db43bbf8e37265bbfc273ab2c5dac055a
dudu2382 <motadossantosjoseeduardo.m82@gmail.com>
2026-02-13 19:37:56 -0300

    modulo terminal criado

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "modules/terminal.h"
 
 #define PRETO    30
 #define VERMELHO 31
@@ -43,42 +44,6 @@ int cores[] = {
     AZUL,
     MAGENTA,
     CIANO
-};
-
-void resetarCor() {
-    // imprime uma sequencia para retornar a cor original
-    printf("\033[0m");
-}
-
-void mudarEstilo(int estilo,int cor){
-    // imprime uma sequencia de estilo e cor
-
-    printf("\033[%d;%dm", estilo, cor);
-}
-
-void moverCursor(int linha, int coluna) {
-    //move o cursor para a linha e coluna desejada
-
-    printf("\033[%d;%dH", linha, coluna);
-}
-
-void titulo(char *tit, int tam, int lin){
-    //imprime um titulo na linha desejada
-    //o tamanho é usado para calcular o centro da tela
-    int coluna = (80-tam)/2;
-
-    moverCursor(lin, coluna);
-
-    printf("\033[%d;%dm", ESTILO_REVERSO, ESTILO_NEGRITO);
-    printf("%s\n", tit);
-    printf("\n");
-    resetarCor();
-}
-
-void limparTela(){
-    //limpa a tela e escreve o titulo do jogo
-    printf("\033[2J");
-    titulo("LIGUE4", 6, 2);
 };
 
 void printMenuInicial(){
---
+++ modules/terminal.h
@@ -0,0 +1,15 @@
+#ifndef TERMINAL_H
+#define TERMINAL_H
+
+#define ESTILO_RESET      0
+#define ESTILO_NEGRITO    1
+#define ESTILO_SUBLINHADO 4
+#define ESTILO_REVERSO    7
+
+void mudarEstilo(int estilo,int cor);
+void resetarCor();
+void moverCursor(int linha, int coluna);
+void titulo(char *tit, int tam, int lin);
+void limparTela();
+
+#endif
